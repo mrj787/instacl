c045274ec33adcc25a36e4263ec67e3f4e235e6c
mohammadrezaj <mohat888@gmail.com>
2020-08-19 18:00:54 +0430

    Updates

---
+++ precache-manifest.9273d8eb8938bc64655467ebaa005b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5485eff0cee5c09304b34c9e40a55e7",
+    "url": "/instacl/index.html"
+  },
+  {
+    "revision": "8840e748a52a305e162f",
+    "url": "/instacl/static/css/main.56112ddf.chunk.css"
+  },
+  {
+    "revision": "66a32d9776125ec1596e",
+    "url": "/instacl/static/js/2.666893df.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instacl/static/js/2.666893df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8840e748a52a305e162f",
+    "url": "/instacl/static/js/main.e1f5aa51.chunk.js"
+  },
+  {
+    "revision": "dc1746c36d081e0187e8",
+    "url": "/instacl/static/js/runtime-main.67b2e5bb.js"
+  }
+]);
